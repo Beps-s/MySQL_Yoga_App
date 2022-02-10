8f9b52153bdeab6f74e44d58c919ce2159a36ada
UwUBeebi <chris.erman@khk.ee>
2022-02-10 16:45:59 +0200

    Added static css and image files

--- index.js
+++ index.js
@@ -2,9 +2,23 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const mysql = require('mysql2')
+const mysql = require('mysql')
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}))
+
+// add template engine
+const hbs = require('express-handlebars');
+// setup template engine directory and files extensions
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine('hbs', hbs.engine({
+	extname: 'hbs',
+	defaultLayout: 'main',
+	layoutsDir: __dirname + '/views/layouts/'
+}))
+
+// setup static public directory
+app.use(express.static('public'));
 
 // create database connection
 var con = mysql.createConnection({
